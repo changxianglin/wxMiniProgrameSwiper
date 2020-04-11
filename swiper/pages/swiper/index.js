ad502064e8d53456c672b1c34b2680c8e1995cf9
changxianglin <281532612@qq.com>
2020-04-12 00:00:10 +0800

    add pro

---
+++ swiper/pages/swiper/index.js
@@ -0,0 +1,72 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    autoplay: true,
+    swiperData: [
+      {
+        id: 1,
+        src: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586630383035&di=21c3ebdee2034abf27e7d948cb908f19&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20181003%2F0f8307fe3de6468d8b51c53b276e9e1b.jpeg"
+      },
+
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
